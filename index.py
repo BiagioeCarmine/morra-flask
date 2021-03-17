b624da7a5526e82c2c820a29ba13be304555c71f
Carmine Zaccagnino <carmine@carminezacc.com>
2021-03-17 21:43:48 +0100

    Fix dell'ultimo momento al setup iniziale per non impazzire quando mi accorgo che non prende le route da matchmaking e users

--- index.py
+++ index.py
@@ -3,6 +3,12 @@ import _routes
 
 app = Flask(__name__)
 app.register_blueprint(_routes.match)
+app.register_blueprint(_routes.matchmaking)
+app.register_blueprint(_routes.users)
+
+@app.route("/")
+def root():
+  return "Non c'è niente da vedere"
 
 if __name__ == "__main__":
     app.run()

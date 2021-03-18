2d83f9bb9cd20689612ad89ccd67112b641d2641
Carmine Zaccagnino <carmine@carminezacc.com>
2021-03-18 21:34:30 +0100

    Forse aggiunto SQLAlchemy funzionante e un po' di spiegazioni

--- _utils/__init__.py
+++ _utils/__init__.py
@@ -1,2 +1,5 @@
 import _utils.users
 import _utils.matches
+import _utils.db
+import _utils.consts
+import _utils.models
--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from _utils.db import init_db
 import _routes
 
 app = Flask(__name__)
+
+init_db()
+
 app.register_blueprint(_routes.match)
 app.register_blueprint(_routes.matchmaking)
 app.register_blueprint(_routes.users)
+
 
 @app.route("/")
 def root():

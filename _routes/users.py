e18e35b73cada13ebb42279d0e5e769e217fb929
Carmine Zaccagnino <carmine@carminezacc.com>
2021-03-19 08:15:10 +0100

    aggiunta route di esempio
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- _routes/users.py
+++ _routes/users.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
-from _utils import users
+from flask import Blueprint, jsonify
+from _utils import models, db
 
 users = Blueprint('users', __name__, url_prefix="/users")
 
 """
 Route usate per gestire gli utenti.
 """
+
+
+@users.route("/", methods=['GET'])
+def get_users():
+    return jsonify(models.User.query.all())

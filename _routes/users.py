015c7bbc22e8d33c6c8f64e30ab490f3fd480279
Carmine Zaccagnino <carmine@carminezacc.com>
2021-03-19 08:32:44 +0100

    aggiunta libreria jwt
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- _routes/users.py
+++ _routes/users.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, jsonify
-from _utils import models, db
+from _utils import models, db, consts
+import jwt
+import os
+
+key_from_env = os.getenv("JWT_KEY")
+jwt_key = consts.JWT_TEST_KEY if key_from_env is None else key_from_env
 
 users = Blueprint('users', __name__, url_prefix="/users")
+
 
 """
 Route usate per gestire gli utenti.
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from _utils.db import init_db
 import _routes
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,9 @@ init_db()
 app.register_blueprint(_routes.match)
 app.register_blueprint(_routes.matchmaking)
 app.register_blueprint(_routes.users)
+
+if os.getenv("JWT_KEY") is None:
+    print("Non è stata impostata una chiave per firmare i JWT, quindi verrà usata quella di test")
 
 
 @app.route("/")

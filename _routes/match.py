5955dcc8777ed7eda5c8dc54b68359fa789e7a6a
Grimos10 <biagiogrimos@gmail.com>
2021-03-25 17:43:51 +0100

    aggiunta route

--- _routes/match.py
+++ _routes/match.py
@@ -8,6 +8,12 @@ Route usate per gestire una singola partita.
 """
 
 
+@match.route("/", methods=['GET'])
+def get_matchs():
+    result = models.Match.query.all()
+    return jsonify([m.jsonify() for m in result])
+
+
 @match.route("/<match_id>", methods=['GET'])
 def get_match(match_id):
     return jsonify(models.Match.query.get(match_id).jsonify())

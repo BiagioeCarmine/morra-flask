e96de17f14ec382416981b134a528667e908206d
Grimos10 <biagiogrimos@gmail.com>
2021-03-26 18:03:55 +0100

    aggiunta funzione set_move

--- _utils/match.py
+++ _utils/match.py
@@ -1,12 +1,15 @@
-from _utils import redis
+from _utils import redis, models
 
 """
 Cose di utilità per far funzionare le route di /match.
 """
 
 
-def set_roba(matchid, userid, hand, prediction):
-    # prendi il match con id id e vedi se id è player1 o player2
-    redis.redis_db.hset("match {} player {}".format(matchid, 1), mapping={'hand': hand, 'prediction': prediction})
-    # TODO:fare roba
-
+def set_move(matchid, userid, hand, prediction):
+    m_id = models.Match.query.get(matchid)
+    if userid == m_id.userid1:
+        redis.redis_db.hset("match {} player {}".format(matchid, 1),
+                            mapping={'hand': hand, 'prediction': prediction})
+    elif userid == m_id.userid2:
+        redis.redis_db.hset("match {} player {}".format(matchid, 2),
+                            mapping={'hand': hand, 'prediction': prediction})

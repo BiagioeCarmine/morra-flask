1253dab3af9f55f3998d828ee37332b283f26a34
Grimos10 <biagiogrimos@gmail.com>
2021-03-30 18:09:44 +0200

    passati dai socket alle route

--- _routes/matches.py
+++ _routes/matches.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
-from _utils import models, socketio, match
-from flask_socketio import Namespace, emit
+from flask import Blueprint, jsonify, request, Response
+from _utils import models, match, consts
+
 import jwt
+import os
+
+key_from_env = os.getenv("JWT_KEY")
+jwt_key = consts.JWT_TEST_KEY if key_from_env is None else key_from_env
+
 
 matches = Blueprint('matches', __name__, url_prefix="/matches")
 
@@ -21,34 +26,37 @@ def get_match(match_id):
     return jsonify(models.Match.query.get(match_id).jsonify())
 
 
-def communicate_match_over(sid):
-    emit("match over", room=sid)
-
-
-class MatchNamespace(Namespace):
-    def on_connect(self):
-        pass
-
-    def on_disconnect(self):
-        """
-        Funzione chiamata quando un utente si
-        disconnette dal socket. La partita
-        deve finire e dobbiamo avvisare
-        l'utente rimasto che la partita
-        è finita e ha vinto.
-        """
-        # emit("the other user disconnected", room=altroutente)
-        pass
-
-    def on_move(self, token, match_id, hand, prediction):
-        """
-        Risponde all'evento "move" sul socket,
-        decodifica il token, prende l'user id
-        e registra la mossa usando match.set_move()
-        # TODO:fai sta cosa Biagio
-        ;:return: se il token è buono, restituirà OK, altrimenti bad token
-        """
-        return "CIAO"
-
-
-socketio.socketio.on_namespace(MatchNamespace('/matches'))
+@matches.route("/<match_id>/move", methods=["POST"])
+def set_move(match_id):
+    """
+    Decodifica il token, prende l'user id
+    e registra la mossa usando match.set_move()
+    ;:return: se il token è buono, restituirà OK, altrimenti bad token
+    """
+    if not request.form:
+        return Response("missing form", status=400)
+    if not request.form.get("hand") is None:
+        return Response("missing hand", status=400)
+    if not request.form.get("prediction") is None:
+        return Response("missing prediction", status=400)
+    try:
+        hand = int(request.form['hand'])
+        if hand < 1 or hand > 5:
+            return Response("bad hand", status=400)
+        prediction = int(request.form['prediction'])
+        if prediction < 2 or prediction > 10:
+            return Response("bad prediction", status=400)
+        token = request.headers.get("Authorization").split("Bearer ")[1]
+        payload = jwt.decode(token, jwt_key, algorithms=["HS256"])
+        print("got jwt ", flush=True)
+        print(payload, flush=True)
+        match.set_move(match_id, payload['id'], hand, prediction)
+        return "OK"
+    except jwt.DecodeError:
+        return "bad token"
+    except ValueError:
+        return Response("hand and prediction weren't integers", status=400)
+    except IndexError:
+        return Response("bad Authorization string", status=400)
+    except AttributeError:
+        return Response("missing Authorization header", status=400)
